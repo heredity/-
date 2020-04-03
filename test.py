3b2cdda657af24282156bb3c4123a495f2731938
heredity <62860507+heredity@users.noreply.github.com>
2020-04-03 01:49:47 -0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,12 @@
+print('Hello, what is your name?')
+name = input()
+print('Hello,', name,'it is so nice to meet you.', end = " ")
+answer_one = input('Would you like coffee? Yes or No? ')
+
+if answer_one == "Yes":
+    print('Here you go, sir, enjoy your coffee', name)
+
+if answer_one == "No":
+    print('Ok. I understand', name)
+
+
